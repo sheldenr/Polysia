9be34921ed9c931fab57d55e3239320bf9ec68e5
Shelden Rattray <sheldenr@unknownbaee63ee98d7.attlocal.net>
2026-03-14 23:57:58 -0400

    Added sign up page

---
+++ client/pages/SignUp.tsx
@@ -0,0 +1,10 @@
+import PlaceholderPage from "./PlaceholderPage";
+
+export default function SignUp() {
+  return (
+    <PlaceholderPage
+      title="Sign Up"
+      description="Create your account and start your language learning journey today!"
+    />
+  );
+}
